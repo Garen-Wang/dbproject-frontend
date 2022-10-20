f2f308da4707cb6a3ce55142c7f69c4b7789d008
Garen Wang <g4r3nwang@gmail.com>
2022-10-20 11:03:29 +0800

    finished basic behaviors of audio player

--- src/components/audio_player.rs
+++ src/components/audio_player.rs
@@ -9,8 +9,12 @@ pub fn audio_player() -> Html {
 
     let audio_ref = use_node_ref();
     let progress_bar_ref = use_node_ref();
+    let volume_bar_ref = use_node_ref();
 
-    let audio = use_media(audio_ref.clone(), "http://commondatastorage.googleapis.com/codeskulptor-demos/DDR_assets/Kangaroo_MusiQue_-_The_Neverwritten_Role_Playing_Game.mp3".to_string());
+    let audio = use_media(
+        audio_ref.clone(),
+        "http://commondatastorage.googleapis.com/codeskulptor-demos/DDR_assets/Kangaroo_MusiQue_-_The_Neverwritten_Role_Playing_Game.mp3".to_string()
+    );
 
     let onclick_play_pause = {
         let is_playing = is_playing.clone();
@@ -21,21 +25,31 @@ pub fn audio_player() -> Html {
             let audio = audio_ref.cast::<HtmlAudioElement>().unwrap();
             if prev_state {
                 log::debug!("pause");
-                audio.pause().unwrap(); // how to handle promise?
+                audio.pause();
             } else {
                 log::debug!("play");
-                let _promise = audio.play().unwrap(); // how to handle promise?
+                audio.play();
             }
         })
     };
 
-    let onchange = {
+    let onchange_progress_bar = {
         let progress_bar_ref = progress_bar_ref.clone();
         let audio = audio.clone();
         Callback::from(move |_| {
             let progress_bar = progress_bar_ref.cast::<HtmlInputElement>().unwrap();
             let cur_time: f64 = progress_bar.value().parse().unwrap();
-            audio.time.set(cur_time);
+            audio.seek(cur_time);
+        })
+    };
+
+    let onchange_volume_bar = {
+        let volume_bar_ref = volume_bar_ref.clone();
+        let audio = audio.clone();
+        Callback::from(move |_| {
+            let volume_bar = volume_bar_ref.cast::<HtmlInputElement>().unwrap();
+            let cur_vol = volume_bar.value().parse::<f64>().unwrap() * 0.01;
+            audio.set_volume(cur_vol);
         })
     };
 
@@ -45,8 +59,7 @@ pub fn audio_player() -> Html {
         Callback::from(move |_| {
             let prev_time = *audio.time as i32;
             let cur_time = (prev_time - 10).max(0);
-            audio.time.set(cur_time as f64);
-
+            audio.seek(cur_time as f64);
             let progress_bar = progress_bar_ref.cast::<HtmlInputElement>().unwrap();
             progress_bar.set_value(&cur_time.to_string());
         })
@@ -59,8 +72,7 @@ pub fn audio_player() -> Html {
             let prev_time = *audio.time as i32;
             let duration = *audio.duration as i32;
             let cur_time = (prev_time + 10).min(duration);
-            audio.time.set(cur_time as f64);
-
+            audio.seek(cur_time as f64);
             let progress_bar = progress_bar_ref.cast::<HtmlInputElement>().unwrap();
             progress_bar.set_value(&cur_time.to_string());
         })
@@ -68,7 +80,7 @@ pub fn audio_player() -> Html {
 
     html! {
         <div>
-            <audio ref={audio_ref}></audio>
+            <audio ref={audio_ref} preload="metadata"></audio>
             <button onclick={backward_ten_secs}>{"10s backward"}</button>
             <button onclick={onclick_play_pause}>
             {
@@ -84,10 +96,14 @@ pub fn audio_player() -> Html {
             <div>{formatted_duration(*audio.time)}</div>
 
             <div>
-                <input ref={progress_bar_ref} type="range" max={((*audio.duration) as i32).to_string()} onchange={onchange} />
+                <input ref={progress_bar_ref} type="range" max={((*audio.duration) as i32).to_string()} value={((*audio.time) as i32).to_string()} onchange={onchange_progress_bar} />
             </div>
 
             <div>{formatted_duration(*audio.duration)}</div>
+
+            <div>
+                <input ref={volume_bar_ref} type="range" max="100" value={((*audio.volume * 100.0) as i32).to_string()} onchange={onchange_volume_bar} />
+            </div>
         </div>
     }
 }
